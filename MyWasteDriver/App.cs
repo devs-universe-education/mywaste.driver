8572723c2b3821a74f47e204e0573a0bc3a460e4
Сергей Селютин <dilast1@outlook.com>
2019-04-13 13:22:47 +0300

    AppCenter

--- MyWasteDriver/App.cs
+++ MyWasteDriver/App.cs
@@ -2,6 +2,9 @@ using MyWasteDriver.DAL.DataServices;
 using MyWasteDriver.UI;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace MyWasteDriver
@@ -18,6 +21,7 @@ namespace MyWasteDriver
 		protected override void OnStart ()
 		{
 			NavigationService.Instance.SetMainPage(AppPages.Login);
+			AppCenter.Start("android=48a4805b-b589-4e89-8776-97b9711f852a;" + "uwp={Your UWP App secret here};" + "ios={Your iOS App secret here}", typeof(Analytics), typeof(Crashes));
 		
 		}
 	}
