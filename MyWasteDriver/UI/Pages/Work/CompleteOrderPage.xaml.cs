fcea45b8fdee64587e41c633fa06988ebd07fd47
Артём Коренев <artemkorenev@mail.ru>
2019-06-06 11:28:34 +0300

    Исправления ui

--- MyWasteDriver/UI/Pages/Work/CompleteOrderPage.xaml.cs
+++ MyWasteDriver/UI/Pages/Work/CompleteOrderPage.xaml.cs
@@ -13,25 +13,8 @@ namespace MyWasteDriver.UI.Pages.Work
 		
 		}
 
-		private void Ckliked(object sender, System.EventArgs e) {
-			
-		}
+		
 
-		//private void FocusGlass(object sender, FocusEventArgs e) {
-		//	var i = 0;
-		//	if (i == (i % 2) * 2) {
-		//		GlassEntr.IsVisible = true;
-		//	}
-		//	else
-		//	GlassEntr.IsVisible = false;
-		//}
-
-		//private void FocusPlastik(object sender, FocusEventArgs e) {
-		//	PlastikEntr.IsVisible = true;
-		//}
-
-		//private void FocusPaper(object sender, FocusEventArgs e) {
-		//	PaperEntr.IsVisible = true;
-		//}
+		
 	}
 }
