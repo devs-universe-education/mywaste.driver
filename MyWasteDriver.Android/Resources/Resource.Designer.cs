b8384b7210bee84b19ff62c20c739d01d89ab170
Артём Коренев <artemkorenev@mail.ru>
2019-07-02 02:20:00 +0300

    Loginpage ui

--- MyWasteDriver.Android/Resources/Resource.Designer.cs
+++ MyWasteDriver.Android/Resources/Resource.Designer.cs
@@ -4580,26 +4580,26 @@ namespace MyWasteDriver.Android
 			// aapt resource value: 0x7f020055
 			public const int avd_hide_password = 2130837589;
 			
-			// aapt resource value: 0x7f02014f
-			public const int avd_hide_password_1 = 2130837839;
-			
 			// aapt resource value: 0x7f020150
-			public const int avd_hide_password_2 = 2130837840;
+			public const int avd_hide_password_1 = 2130837840;
 			
 			// aapt resource value: 0x7f020151
-			public const int avd_hide_password_3 = 2130837841;
+			public const int avd_hide_password_2 = 2130837841;
+			
+			// aapt resource value: 0x7f020152
+			public const int avd_hide_password_3 = 2130837842;
 			
 			// aapt resource value: 0x7f020056
 			public const int avd_show_password = 2130837590;
 			
-			// aapt resource value: 0x7f020152
-			public const int avd_show_password_1 = 2130837842;
-			
 			// aapt resource value: 0x7f020153
-			public const int avd_show_password_2 = 2130837843;
+			public const int avd_show_password_1 = 2130837843;
 			
 			// aapt resource value: 0x7f020154
-			public const int avd_show_password_3 = 2130837844;
+			public const int avd_show_password_2 = 2130837844;
+			
+			// aapt resource value: 0x7f020155
+			public const int avd_show_password_3 = 2130837845;
 			
 			// aapt resource value: 0x7f020057
 			public const int common_full_open_on_phone = 2130837591;
@@ -5309,11 +5309,11 @@ namespace MyWasteDriver.Android
 			// aapt resource value: 0x7f020142
 			public const int notification_icon_background = 2130837826;
 			
-			// aapt resource value: 0x7f02014d
-			public const int notification_template_icon_bg = 2130837837;
-			
 			// aapt resource value: 0x7f02014e
-			public const int notification_template_icon_low_bg = 2130837838;
+			public const int notification_template_icon_bg = 2130837838;
+			
+			// aapt resource value: 0x7f02014f
+			public const int notification_template_icon_low_bg = 2130837839;
 			
 			// aapt resource value: 0x7f020143
 			public const int notification_tile_bg = 2130837827;
@@ -5344,6 +5344,9 @@ namespace MyWasteDriver.Android
 			
 			// aapt resource value: 0x7f02014c
 			public const int tooltip_frame_light = 2130837836;
+			
+			// aapt resource value: 0x7f02014d
+			public const int wasterecycling = 2130837837;
 			
 			static Drawable()
 			{
