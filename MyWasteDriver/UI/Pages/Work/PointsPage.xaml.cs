c73b9c157436ee4cddc5308372af16f0b6b8f789
dilast1 <46301733+dilast1@users.noreply.github.com>
2019-07-02 23:40:47 +0300

    Изменения в экране PointsPage

--- MyWasteDriver/UI/Pages/Work/PointsPage.xaml.cs
+++ MyWasteDriver/UI/Pages/Work/PointsPage.xaml.cs
@@ -10,24 +10,24 @@ using Xamarin.Forms.Xaml;
 namespace MyWasteDriver.UI.Pages.Work {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class PointsPage: BasePage {
-		[Obsolete]
+		//[Obsolete]
 		public PointsPage() {
 			InitializeComponent();
-			var tb = new ToolbarItem {
-				Text="16 июля 2019",
-				Order= ToolbarItemOrder.Primary,
-				Priority = 0,
+		//	var tb = new ToolbarItem {
+		//		Text="16 июля 2019",
+		//		Order= ToolbarItemOrder.Primary,
+		//		Priority = 0,
 				
-			};
-			var tb2 = new ToolbarItem {
-				Order = ToolbarItemOrder.Primary,
-				Priority = 2,
-				Icon = new FileImageSource {
-					File = "replay.png"
-				}
-			};
-			ToolbarItems.Add(tb);
-			ToolbarItems.Add(tb2);
+		//	};
+		//	var tb2 = new ToolbarItem {
+		//		Order = ToolbarItemOrder.Primary,
+		//		Priority = 2,
+		//		Icon = new FileImageSource {
+		//			File = "replay.png"
+		//		}
+		//	};
+		//	ToolbarItems.Add(tb);
+		//	ToolbarItems.Add(tb2);
 		}
 	}
 }
