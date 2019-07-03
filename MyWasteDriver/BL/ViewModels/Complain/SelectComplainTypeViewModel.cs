4f3a99f62d535998b75274c06f24151b7f451df7
MrsterKek <49153797+MrsterKek@users.noreply.github.com>
2019-07-04 02:18:35 +0300

    Select Complain Экран 3.1

--- MyWasteDriver/BL/ViewModels/Complain/SelectComplainTypeViewModel.cs
+++ MyWasteDriver/BL/ViewModels/Complain/SelectComplainTypeViewModel.cs
@@ -4,11 +4,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
+
+
 namespace MyWasteDriver.BL.ViewModels.Complain {
 	class SelectComplainTypeViewModel : BaseViewModel {
+
+		
 		public ICommand GoToReportComplainCommand => GetNavigateToCommand(AppPages.ReportComplain);
+
 		public override async Task OnPageAppearing() {
 			State = PageState.Normal;
+			
 		}
+		
 	}
 }
