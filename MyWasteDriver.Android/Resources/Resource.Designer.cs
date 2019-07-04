c9ff0f34f728c42e80896a234b5288c630dba5ae
MrsterKek <49153797+MrsterKek@users.noreply.github.com>
2019-07-04 03:44:03 +0300

    Report Complain страница 3.2

--- MyWasteDriver.Android/Resources/Resource.Designer.cs
+++ MyWasteDriver.Android/Resources/Resource.Designer.cs
@@ -5834,26 +5834,26 @@ namespace MyWasteDriver.Android
 			// aapt resource value: 0x7f020057
 			public const int avd_hide_password = 2130837591;
 			
-			// aapt resource value: 0x7f02015e
-			public const int avd_hide_password_1 = 2130837854;
-			
 			// aapt resource value: 0x7f02015f
-			public const int avd_hide_password_2 = 2130837855;
+			public const int avd_hide_password_1 = 2130837855;
 			
 			// aapt resource value: 0x7f020160
-			public const int avd_hide_password_3 = 2130837856;
+			public const int avd_hide_password_2 = 2130837856;
+			
+			// aapt resource value: 0x7f020161
+			public const int avd_hide_password_3 = 2130837857;
 			
 			// aapt resource value: 0x7f020058
 			public const int avd_show_password = 2130837592;
 			
-			// aapt resource value: 0x7f020161
-			public const int avd_show_password_1 = 2130837857;
-			
 			// aapt resource value: 0x7f020162
-			public const int avd_show_password_2 = 2130837858;
+			public const int avd_show_password_1 = 2130837858;
 			
 			// aapt resource value: 0x7f020163
-			public const int avd_show_password_3 = 2130837859;
+			public const int avd_show_password_2 = 2130837859;
+			
+			// aapt resource value: 0x7f020164
+			public const int avd_show_password_3 = 2130837860;
 			
 			// aapt resource value: 0x7f020059
 			public const int check = 2130837593;
@@ -6590,11 +6590,11 @@ namespace MyWasteDriver.Android
 			// aapt resource value: 0x7f02014d
 			public const int notification_icon_background = 2130837837;
 			
-			// aapt resource value: 0x7f02015c
-			public const int notification_template_icon_bg = 2130837852;
-			
 			// aapt resource value: 0x7f02015d
-			public const int notification_template_icon_low_bg = 2130837853;
+			public const int notification_template_icon_bg = 2130837853;
+			
+			// aapt resource value: 0x7f02015e
+			public const int notification_template_icon_low_bg = 2130837854;
 			
 			// aapt resource value: 0x7f02014e
 			public const int notification_tile_bg = 2130837838;
@@ -6603,40 +6603,43 @@ namespace MyWasteDriver.Android
 			public const int notify_panel_notification_icon_bg = 2130837839;
 			
 			// aapt resource value: 0x7f020150
-			public const int places_ic_clear = 2130837840;
+			public const int photo = 2130837840;
 			
 			// aapt resource value: 0x7f020151
-			public const int places_ic_search = 2130837841;
+			public const int places_ic_clear = 2130837841;
 			
 			// aapt resource value: 0x7f020152
-			public const int powered_by_google_dark = 2130837842;
+			public const int places_ic_search = 2130837842;
 			
 			// aapt resource value: 0x7f020153
-			public const int powered_by_google_light = 2130837843;
+			public const int powered_by_google_dark = 2130837843;
 			
 			// aapt resource value: 0x7f020154
-			public const int remove = 2130837844;
+			public const int powered_by_google_light = 2130837844;
 			
 			// aapt resource value: 0x7f020155
-			public const int replay = 2130837845;
+			public const int remove = 2130837845;
 			
 			// aapt resource value: 0x7f020156
-			public const int roundedbg = 2130837846;
+			public const int replay = 2130837846;
 			
 			// aapt resource value: 0x7f020157
-			public const int roundedbgdark = 2130837847;
+			public const int roundedbg = 2130837847;
 			
 			// aapt resource value: 0x7f020158
-			public const int tooltip_frame_dark = 2130837848;
+			public const int roundedbgdark = 2130837848;
 			
 			// aapt resource value: 0x7f020159
-			public const int tooltip_frame_light = 2130837849;
+			public const int tooltip_frame_dark = 2130837849;
 			
 			// aapt resource value: 0x7f02015a
-			public const int truck = 2130837850;
+			public const int tooltip_frame_light = 2130837850;
 			
 			// aapt resource value: 0x7f02015b
-			public const int wasterecycling = 2130837851;
+			public const int truck = 2130837851;
+			
+			// aapt resource value: 0x7f02015c
+			public const int wasterecycling = 2130837852;
 			
 			static Drawable()
 			{
