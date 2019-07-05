2e3ca478dc13624f04172c80e4228977bd425181
MrsterKek <49153797+MrsterKek@users.noreply.github.com>
2019-07-05 03:20:47 +0300

    Коректировка карт

--- MyWasteDriver/BL/ViewModels/Complain/SelectComplainTypeViewModel.cs
+++ MyWasteDriver/BL/ViewModels/Complain/SelectComplainTypeViewModel.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using TK.CustomMap;
 
 namespace MyWasteDriver.BL.ViewModels.Complain {
 	class SelectComplainTypeViewModel : BaseViewModel {
+
+		public MapSpan Position { get; private set; }
 		public ICommand GoToReportComplainCommand => GetNavigateToCommand(AppPages.ReportComplain);
 		public override async Task OnPageAppearing() {
 			State = PageState.Normal;
+			Position = new MapSpan(new Position(51.712468, 39.181733), 0.2, 0.2);
 		}
 	}
 }
