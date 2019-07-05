c45dc9759cf420529da2772ea539f0f1ce961930
dilast1 <46301733+dilast1@users.noreply.github.com>
2019-07-05 13:36:24 +0300

    доабвлена возможность добавить фото

--- MyWasteDriver/BL/ViewModels/Complain/ReportComplainViewModel.cs
+++ MyWasteDriver/BL/ViewModels/Complain/ReportComplainViewModel.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
 using TK.CustomMap;
+using Xamarin.Forms;
 
 namespace MyWasteDriver.BL.ViewModels.Complain {
 	class ReportComplainViewModel : BaseViewModel {
@@ -13,7 +16,37 @@ namespace MyWasteDriver.BL.ViewModels.Complain {
 
 		public ICommand ReturnToPoints => GetNavigateToCommand(AppPages.Points);
 
-		public override async Task OnPageAppearing() {
+		public ICommand MakePhotoCommand => MakeCommand(MakePhoto);
+
+		private Image img = new Image();
+
+		public string PhotoB
+		{
+			get => Get<string>();
+			set => Set(value);
+		} 
+
+		private async void MakePhoto()
+		{
+			if (CrossMedia.Current.IsCameraAvailable && CrossMedia.Current.IsTakePhotoSupported) {
+				var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions {
+					SaveToAlbum = true,
+					Directory = "Sample",
+					Name = $"{DateTime.Now.ToString("dd.MM.yyyy_hh.mm.ss")}.jpg"
+				});
+
+				if (file == null)
+					return;
+
+				PhotoB = file.Path;
+			}
+		}
+
+
+
+		public override async Task OnPageAppearing()
+		{
+			PhotoB = "photo.jpg";
 			State = PageState.Normal;
 			Position = new MapSpan(new Position(51.712468, 39.181733), 0.2, 0.2);
 		}
